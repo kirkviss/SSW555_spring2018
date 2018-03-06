a66acf1488e80be658a92f5124aa4f3c50adb71a
Max Remetz <max@remetz.com>
2018-03-05 19:34:43 -0500

    fixed tab issue

--- gedParser.py
+++ gedParser.py
@@ -162,7 +162,7 @@ def gedcomParser():
         marriageBeforeDeath(k, v[Marriage],v[HusbandId], individuals[v[HusbandId]][Death], v[WifeId], individuals[v[WifeId]][Death])
         divorceBeforeDeath(k, v[Divorce], v[HusbandId], individuals[v[HusbandId]][Death], v[WifeId], individuals[v[WifeId]][Death])
         birthBeforeMarriage(k, v[Marriage], v[HusbandId], individuals[v[HusbandId]][Birthday], v[WifeId], individuals[v[WifeId]][Birthday])
-	fewerThanFifteen(k,v["Children"],v["Husband Name"],v["Wife Name"])
+        fewerThanFifteen(k,v["Children"],v["Husband Name"],v["Wife Name"])
 
         
         for child in v['Children']: 
